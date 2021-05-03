cf95e12e1f4cdff2b509695640549404bfa15277
Jesus Sánchez <jesus.sanchez@in-storemedia.com>
2021-05-02 20:58:32 -0500

    feat: add example with TypeScript

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,13 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Jesús';
+let hp: number | string = 95;
+let estaVivo : boolean = false;
+
+hp = 77;
+
+nombre = 'Chui2';
+
+console.log(nombre, hp);
