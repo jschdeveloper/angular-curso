381cb7542e6e431fc82979d62b74fc818374e79b
Jesus Sánchez <jesus.sanchez@in-storemedia.com>
2021-05-03 17:07:35 -0500

    feat: add objects types in ts. [class 2]

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,3 @@
     ===== Código de TypeScript =====
 */
 
-let nombre: string = 'Jesús';
-let hp: number | string = 95;
-let estaVivo : boolean = false;
-
-hp = 77;
-
-nombre = 'Chui2';
-
-console.log(nombre, hp);
