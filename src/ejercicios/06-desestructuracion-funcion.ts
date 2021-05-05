12a5235e5c773ee2917744cd2af482f334c5005d
Jesus Sánchez <jesus.sanchez@in-storemedia.com>
2021-05-04 21:20:25 -0500

    chore: import and export [class 7]

--- src/ejercicios/06-desestructuracion-funcion.ts
+++ src/ejercicios/06-desestructuracion-funcion.ts
@@ -1,4 +1,4 @@
-interface Producto {
+export interface Producto {
     desc: string;
     precio: number;
 
@@ -14,7 +14,7 @@ const tableta: Producto = {
     precio: 350
 }
 
-function calculaISV(productos: Producto[]): [number, number] {
+export function calculaISV(productos: Producto[]): [number, number] {
     let total = 0;
 
     //productos.forEach((producto) => { //sin desestructurar
---
+++ src/ejercicios/07-import-export.ts
@@ -0,0 +1,21 @@
+// import { Producto, calculaISV } from "./ejercicios/06-desestructuracion-funcion";
+import { Producto, calculaISV } from "./06-desestructuracion-funcion";
+
+
+const carritoCompras: Producto[] = [
+    {
+        desc: 'Telefono 1',
+        precio: 100
+    },
+    {
+        desc: 'Telefono 1',
+        precio: 150
+    }
+];
+
+const [total, isv] = calculaISV(carritoCompras);
+
+console.log('Total ', total);
+console.log('ISV ', isv);
+
+
